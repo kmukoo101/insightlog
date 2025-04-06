1e68b2ce002f50fcb6f108914b9b7fd8cb39d43c
kmukoo101 <70552810+kmukoo101@users.noreply.github.com>
2025-04-05 20:41:06 -0400

    Update setup.py
    name change for pypi package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="insightlog",
+    name="insightlog101",
     version="0.1.0",
     author="kmukoo101",
     description="Secure, redactable, context-aware logger for security and DevOps workflows.",
